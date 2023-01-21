f7254a5eeba76e7e562a151cc9c8ed3c71e05537
shvrekhan <shvrekhan@gmail.com>
2023-01-21 15:52:16 +0530

    card structure fix

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -14,7 +14,7 @@ class Card extends React.Component {
                     <div className="card-category">{this.props.category}</div>
                     <div className="card-title">{this.props.title}</div>
                     <div className="rate-count"> Count ({this.props.count}) & Rating({this.props.rate})</div>
-                    <div className="Price">{this.props.price}</div>
+                    <div className="price">Price:- ${this.props.price}</div>
                     <div className="button-container">
                         <button>Buy Now</button>
                         <button>Add To Cart</button>
